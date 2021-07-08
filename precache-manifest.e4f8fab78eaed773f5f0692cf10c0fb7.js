594cd7e0fde6d9873df7b6897ec3c917ffee5e40
Fenomen-Alex <alexfenomen@gmail.com>
2021-07-08 20:15:42 +0300

    Updates

---
+++ precache-manifest.e4f8fab78eaed773f5f0692cf10c0fb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c03e3a2febd29dfb4b6840e457b17e41",
+    "url": "/Realtime_Search/index.html"
+  },
+  {
+    "revision": "d1693acabeb1f87237cd",
+    "url": "/Realtime_Search/static/css/main.9293805b.chunk.css"
+  },
+  {
+    "revision": "95f3017b0af67f0f1456",
+    "url": "/Realtime_Search/static/js/2.04638505.chunk.js"
+  },
+  {
+    "revision": "12f7f25928149bd3581079eb8a935479",
+    "url": "/Realtime_Search/static/js/2.04638505.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1693acabeb1f87237cd",
+    "url": "/Realtime_Search/static/js/main.d804c27f.chunk.js"
+  },
+  {
+    "revision": "1b1e20299f20900309e7",
+    "url": "/Realtime_Search/static/js/runtime-main.a7e2ec8f.js"
+  }
+]);
